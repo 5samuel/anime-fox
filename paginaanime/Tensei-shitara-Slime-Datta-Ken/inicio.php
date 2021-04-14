9d54cd0a6e13d0aba0eedce2e3d7290ebe4cf4c2
samuelangeles <samueljoseangelereye@gmail.com>
2021-04-14 09:12:13 -0600

    agregacion del anime slime

---
+++ paginaanime/Tensei-shitara-Slime-Datta-Ken/inicio.php
@@ -0,0 +1,306 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<title>Peliculas</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="Shortcut Icon" type="image/x-icon" href="assets/icons/logoo.png" />
+    <script src="js/sweet-alert.min.js"></script>
+    <link rel="stylesheet" href="../../css/sweet-alert.css">
+    <link rel="stylesheet" href="../../css/estado.css">
+    <link rel="stylesheet" href="../../css/img.css">
+    <link rel="stylesheet" href="../../css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" href="../../css/normalize.css">
+    <link rel="stylesheet" href="../../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../css/jquery.mCustomScrollbar.css">
+    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../../css/video.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/jquery-1.11.2.min.js"><\/script>')</script>
+    <script src="../../js/modernizr.js"></script>
+    <script src="../../js/bootstrap.min.js"></script>
+    <script src="../../js/jquery.mCustomScrollbar.concat.min.js"></script>
+    <script src="../../js/main.js"></script>
+</head>
+<body background-color=" #ffdd90">
+    <div class="navbar-lateral full-reset">
+        <div class="visible-xs font-movile-menu mobile-menu-button"></div>
+        <div class="full-reset container-menu-movile custom-scroll-containers">
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+            <div class="full-reset" style="background-color:#2B3D51; padding: 10px 0; color:#fff;">
+                <figure>
+                    <img src="assets/img/logoo.png" alt="Biblioteca" class="img-responsive center-box" style="width:55%;">
+                </figure>
+                <p class="text-center" style="padding-top: 15px;"><font size="4" face="Algerian">the daily life of the immortal king</font></p>
+            </div>
+            <div class="full-reset nav-lateral-list-menu">
+                <ul class="list-unstyled">
+                    <li><a href="http://localhost/samuel/trabajo/peliculas/inicio.html"><i class="zmdi zmdi-home zmdi-hc-fw"></i>&nbsp;&nbsp; Inicio</a></li>
+                    <li>
+                        <div class="dropdown-menu-button"><i class="zmdi zmdi-movie"></i>&nbsp;&nbsp; Animes <i class="zmdi zmdi-chevron-down pull-right zmdi-hc-fw"></i></div>
+                        <ul class="list-unstyled">
+                           <li><a href="../demon-King-cademy/inicio.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Demon King academy</a></li>
+                            <li><a href=""><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Horimiya </a></li>
+                            <li><a href="esperacomedia.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Mortal </a></li>
+                            <li><a href="esperafantasía.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; No game no life</a></li>
+                            <li><a href="esperamelodrama.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; One punch man</a></li>
+                            <li><a href="esperamusical.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; One punch man</a></li>
+                            <li><a href="esperaromance.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Isekai spmarphone</a></li>
+                            <li><a href="esperasuspense.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp;Tatoeba</a></li>
+                            <li><a href="esperaterror.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Terror </a></li>
+                        </ul>
+                    </li>
+                    	 
+
+                    	 <li><a href="desconectar.php"><i class="zmdi zmdi-movie"></i>&nbsp;&nbsp;  Cerrar</a></li>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="content-page-container full-reset custom-scroll-containers">
+
+    	 <div class="footer-copyright full-reset all-tittles">Anime fox</div>
+
+        <div class="container">
+            <div class="page-header">
+              <h1 class="all-tittles"><center><font size="10" face="Algerian">Tensei shitara Slime Datta Ken</font></center></h1>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <ul class="nav nav-tabs nav-justified"  style="font-size: 17px;">
+
+            </ul>
+        </div>
+        <div class="container-fluid"  style="margin: 50px 0;">
+            <div class="row">
+                <div class="col-xs-12 col-sm-4 col-md-3" style="width:30%; border-radius: 25px;">
+                    <img src="../../img/slime.jpg" alt="user" class="img-responsive center-box" style="max-width: 300px;">
+                    <br>
+                    <div class="estado"><center><h4>Finalizado</h4></center></div> <div>
+                        <a href="#" class="btn" id="reaccion"><i class="fas fa-heart"></i></a>
+                        </div>
+                </div>
+                <div class="col-xs-12 col-sm-8 col-md-8 text-justify lead">
+                   <h2>sipnosis</h2>
+                    <p>Satoru Mikami es un hombre de 37 años que tiene un trabajo que no le gusta, sin salida y que no es feliz con la vida que lleva, pero cuando muere a manos de un ladrón y piensa que es su fin, se despierta descubriendo que se ha reencarnado en un mundo de magia y espada… ¡pero como un slime! Ahora tendrá que acostumbrarse a su nueva vida, aunque por suerte contará con dos habilidades únicas que le ayudarán a sobrevivir: una que le proporciona una gran comprensión de todo lo que le rodea, y otra que le permite copiar las habilidades de sus oponentes</p>
+<center>
+<h3>Título original: Tensei shitara Slime Datta Ken</h3>
+<h3>Año:2019</h3>
+<h3>País: Japon</h3>
+<h3>Dirección: Zhu BeNing</h3>
+<h3>Fotografía: Animación</h3>
+<h3>Reparto: Animación</h3>
+
+</center>
+
+                    </p>
+                </div>
+            </div>
+        </div>
+        
+        <div class="container-fluid">
+            <center>
+<div class="container-flat-form" style="
+width:80% ;
+    background: white ;
+    color: white;
+    padding: 20px;
+    margin: 5%;
+    border-radius: 20px;
+    box-shadow: 0px 0px 5px black;
+    ">
+<center>
+<a href="1.php" style="color:black;">
+<img src="https://cdn.animeflv.net/screenshots/3016/1/th_3.jpg" class="imgc"><h3 style="color:black;">Tensei shitara Slime Datta Ken </h3>
+capitulo 1
+</a>
+</center>
+<br>
+<center>
+<a href="2.php" style="color:black;">
+<img src="https://cdn.animeflv.net/screenshots/3016/2/th_3.jpg" class="imgc"><h3>Tensei shitara Slime Datta Ken </h3>
+capitulo 2
+</a>
+</center>
+
+<br>
+<center>
+<a href="3.php" style="color:black;">
+<img src="https://cdn.animeflv.net/screenshots/3016/3/th_3.jpg" class="imgc"><h3>Tensei shitara Slime Datta Ken </h3>
+capitulo 3
+</a>
+</center>
+<br>
+<center>
+<a href="4.php" style="color:black;">
+<img src="https://cdn.animeflv.net/screenshots/3016/4/th_3.jpg" class="imgc"><h3>Tensei shitara Slime Datta Ken </h3>
+capitulo 4
+</a>
+</center>
+<br>
+<center>
+<a href="5.php" style="color:black;">
+<img src="https://cdn.animeflv.net/screenshots/3016/5/th_3.jpg" class="imgc"><h3>Tensei shitara Slime Datta Ken </h3>
+capitulo 5
+</a>
+</center>
+<br>
+<center>
+<a href="6.php" style="color:black;">
+<img src="https://cdn.animeflv.net/screenshots/3016/6/th_3.jpg" class="imgc"><h3>Tensei shitara Slime Datta Ken </h3>
+capitulo 6
+</a>
+</center>
+<br>
+<center>
+<a href="7.php" style="color:black;">
+<img src="https://cdn.animeflv.net/screenshots/3016/7/th_3.jpg" class="imgc"><h3>Tensei shitara Slime Datta Ken </h3>
+capitulo 7
+</a>
+</center>
+<br>
+<center>
+<a href="8.php" style="color:black;">
+<img src="https://cdn.animeflv.net/screenshots/3016/8/th_3.jpg" class="imgc"><h3>Tensei shitara Slime Datta Ken </h3>
+capitulo 8
+</a>
+</center>
+<br>
+<center>
+<a href="9.php" style="color:black;">
+<img src="https://cdn.animeflv.net/screenshots/3016/9/th_3.jpg" class="imgc"><h3>Tensei shitara Slime Datta Ken </h3>
+capitulo 9
+</a>
+</center>
+<br>
+<center>
+<a href="10.php" style="color:black;">
+<img src="https://cdn.animeflv.net/screenshots/3016/10/th_3.jpg" class="imgc"><h3>Tensei shitara Slime Datta Ken </h3>
+capitulo 10
+</a>
+</center>
+<br>
+<center>
+<a href="11.php" style="color:black;">
+<img src="https://cdn.animeflv.net/screenshots/3016/11/th_3.jpg" class="imgc"><h3>Tensei shitara Slime Datta Ken </h3>
+capitulo 11
+</a>
+</center>
+<br>
+<center>
+<a href="12.php" style="color:black;">
+<img src="https://cdn.animeflv.net/screenshots/3016/12/th_3.jpg" class="imgc"><h3>Tensei shitara Slime Datta Ken </h3>
+capitulo 12
+</a>
+</center>
+<br>
+<center>
+<a href="13.php" style="color:black;" >
+<img src="https://cdn.animeflv.net/screenshots/3016/13/th_3.jpg" class="imgc"><h3>Tensei shitara Slime Datta Ken </h3>
+capitulo 13
+</a>
+</center>
+
+<center>
+<a href="14.php" style="color:black;" >
+<img src="https://cdn.animeflv.net/screenshots/3016/14/th_3.jpg" class="imgc"><h3>Tensei shitara Slime Datta Ken </h3>
+capitulo 14
+</a>
+</center>
+
+<center>
+<a href="15.php" style="color:black;" >
+<img src="https://cdn.animeflv.net/screenshots/3016/15/th_3.jpg" class="imgc"><h3>Tensei shitara Slime Datta Ken </h3>
+capitulo 15
+</a>
+</center>
+
+<center>
+<a href="16.php" style="color:black;" >
+<img src="https://cdn.animeflv.net/screenshots/3016/16/th_3.jpg" class="imgc"><h3>Tensei shitara Slime Datta Ken </h3>
+capitulo 16
+</a>
+</center>
+<center>
+<a href="17.php" style="color:black;" >
+<img src="https://cdn.animeflv.net/screenshots/3016/17/th_3.jpg" class="imgc"><h3>Tensei shitara Slime Datta Ken </h3>
+capitulo 17
+</a>
+</center>
+
+<center>
+<a href="18.php" style="color:black;" >
+<img src="https://cdn.animeflv.net/screenshots/3016/18/th_3.jpg" class="imgc"><h3>Tensei shitara Slime Datta Ken </h3>
+capitulo 18
+</a>
+</center>
+
+<center>
+<a href="19.php" style="color:black;" >
+<img src="https://cdn.animeflv.net/screenshots/3016/19/th_3.jpg" class="imgc"><h3>Tensei shitara Slime Datta Ken </h3>
+capitulo 19
+</a>
+</center>
+<center>
+<a href="20.php" style="color:black;" >
+<img src="https://cdn.animeflv.net/screenshots/3016/20/th_3.jpg" class="imgc"><h3>Tensei shitara Slime Datta Ken </h3>
+capitulo 20
+</a>
+</center>
+
+<center>
+<a href="21.php" style="color:black;" >
+<img src="https://cdn.animeflv.net/screenshots/3016/21/th_3.jpg" class="imgc"><h3>Tensei shitara Slime Datta Ken </h3>
+capitulo 21
+</a>
+</center>
+
+<center>
+<a href="22.php" style="color:black;" >
+<img src="https://cdn.animeflv.net/screenshots/3016/22/th_3.jpg" class="imgc"><h3>Tensei shitara Slime Datta Ken </h3>
+capitulo 22
+</a>
+</center>
+
+<center>
+<a href="23.php" style="color:black;" >
+<img src="https://cdn.animeflv.net/screenshots/3016/23/th_3.jpg" class="imgc"><h3>Tensei shitara Slime Datta Ken </h3>
+capitulo 23
+</a>
+</center>
+
+<center>
+<a href="24.php" style="color:black;" >
+<img src="https://cdn.animeflv.net/screenshots/3016/24/th_3.jpg" class="imgc"><h3>Tensei shitara Slime Datta Ken </h3>
+capitulo 24
+</a>
+</center>
+
+<center>
+<a href="25.php" style="color:black;" >
+<img src="https://cdn.animeflv.net/screenshots/3016/25/th_3.jpg" class="imgc"><h3>Tensei shitara Slime Datta Ken </h3>
+capitulo 25
+</a>
+</center>
+</div>
+
+
+
+</center>
+
+        </div>
+        <footer class="footer full-reset">
+
+            <div class="footer-copyright full-reset all-tittles">Mundo anime</div>
+        </footer>
+    </div>
+</body>
+</html>
