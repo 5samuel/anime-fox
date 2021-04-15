9b3acc22c8c8d2596e1b41a08f273901e436984a
samuelangeles <samueljoseangelereye@gmail.com>
2021-04-15 15:38:17 -0600

    Funciones del buscador en tiempo real

--- buscador/js/search.js
+++ buscador/js/search.js
@@ -1,12 +1,12 @@
-var buscador = $("#table").DataTable();
+var buscador = $("#table").DataTable();// referencia al input del buscador.
 
 $("#input-search").keyup(function(){
     
-    buscador.search($(this).val()).draw();
+    buscador.search($(this).val()).draw(); // busquedad en tiempo real.
     
     if ($("#input-search").val() == ""){
-        $(".content-search").fadeOut(300);
+        $(".content-search").fadeOut(300);// ocultar las obciones que no busca.
     }else{
-        $(".content-search").fadeIn(300);
+        $(".content-search").fadeIn(300); // dejar las busquedad que funcionen.
     }
 })
