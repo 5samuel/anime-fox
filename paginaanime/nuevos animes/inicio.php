6f4a4a4451019e727f755f772ccc347073c2c154
JeffersonHidalgo <jeffersonhidalgo@gmail.com>
2021-04-15 00:06:34 -0400

    Agregar Carpeta News Y comentarios.

---
+++ paginaanime/nuevos animes/inicio.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <!--titulo del anime en la pestaña-->
+<title>anime</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="Shortcut Icon" type="image/x-icon" href="assets/icons/logoo.png" />
+    <script src="js/sweet-alert.min.js"></script>
+    <link rel="stylesheet" href="../../css/sweet-alert.css">
+    <link rel="stylesheet" href="../../css/estado.css">
+    <link rel="stylesheet" href="../../css/img.css">
+    <link rel="stylesheet" href="../../css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" href="../../css/normalize.css">
+    <link rel="stylesheet" href="../../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../css/jquery.mCustomScrollbar.css">
+    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../../css/video.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/jquery-1.11.2.min.js"><\/script>')</script>
+    <script src="../../js/modernizr.js"></script>
+    <script src="../../js/bootstrap.min.js"></script>
+    <script src="../../js/jquery.mCustomScrollbar.concat.min.js"></script>
+    <script src="../../js/main.js"></script>
+</head>
+<body background-color=" #ffdd90">
+    <div class="navbar-lateral full-reset">
+        <div class="visible-xs font-movile-menu mobile-menu-button"></div>
+        <div class="full-reset container-menu-movile custom-scroll-containers">
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+            <div class="full-reset" style="background-color:#2B3D51; padding: 10px 0; color:#fff;">
+                <figure>
+                    <img src="../../assets/img/logoo.png" alt="Biblioteca" class="img-responsive center-box" style="width:55%;">
+                </figure>
+                <p class="text-center" style="padding-top: 15px;"><font size="4" face="Algerian">the daily life of the immortal king</font></p>
+            </div>
+            <div class="full-reset nav-lateral-list-menu">
+                <ul class="list-unstyled">
+                    <li><a href="http://localhost/samuel/trabajo/peliculas/inicio.html"><i class="zmdi zmdi-home zmdi-hc-fw"></i>&nbsp;&nbsp; Inicio</a></li>
+                    <li>
+                        <div class="dropdown-menu-button"><i class="zmdi zmdi-movie"></i>&nbsp;&nbsp; Animes <i class="zmdi zmdi-chevron-down pull-right zmdi-hc-fw"></i></div>
+                        <ul class="list-unstyled">
+                        <li><a href="../demon-King-cademy/inicio.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Demon King academy</a></li>
+                            <li><a href="../mortal/inicio.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Mortal </a></li>
+                            <li><a href="../smarfon/inicio.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Isikai </a></li>
+                            <li><a href="../Horimiya/inicio.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Horimoya</a></li>
+                            <li><a href="esperamelodrama.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Melodrama</a></li>
+                            <li><a href="esperamusical.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Musical</a></li>
+                            <li><a href="esperaromance.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Romance</a></li>
+                            <li><a href="esperasuspense.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Suspense</a></li>
+                            <li><a href="esperaterror.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Terror </a></li>
+                        </ul>
+                    </li>
+                    	 <li><a href="espera.php"><i class="zmdi zmdi-movie"></i>&nbsp;&nbsp;  Nuevas Peliculas</a></li>
+
+                    	 <li><a href="desconectar.php"><i class="zmdi zmdi-movie"></i>&nbsp;&nbsp;  Cerrar</a></li>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="content-page-container full-reset custom-scroll-containers">
+
+<!-- Nombre del anime en pantalla-->
+
+    	 <div class="footer-copyright full-reset all-tittles">Mundo anime</div>
+
+        <div class="container">
+            <div class="page-header">
+              <h1 class="all-tittles"><center><font size="10" face="Algerian">Nombre del anime en pantalla</font></center></h1>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <ul class="nav nav-tabs nav-justified"  style="font-size: 17px;">
+
+            </ul>
+        </div>
+        <div class="container-fluid"  style="margin: 50px 0;">
+            <div class="row">
+                <div class="col-xs-12 col-sm-4 col-md-3" style="width:30%; border-radius: 25px;">
+                    <img src="../../img/5.jpg" alt="user" class="img-responsive center-box" style="max-width: 200px;">
+                    <br>
+                    <div class="estado"><center><h4>Finalizado</h4></center></div> <div class="imagen"><img src="../../img/megusta.png" alt=""></div>
+                </div>
+                <div class="sip"><center> <h2 >sipnosis</h2></div></center>
+                <div class="col-xs-12 col-sm-8 col-md-8 text-justify lead">
+
+                  <!--sipnosis del Anime donde dice aqui-->
+                    <p>Aquí<center>
+                        <!-- Datos del Anime-->
+<h3>Título original</h3>
+<h3>No Game, No Life (TV Series)</h3>
+<h3>Año:2014</h3>
+<h3>Duración: 25 min.</h3>
+<h3>País: Japón Japón</h3>
+</center>
+
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid">
+<div class="container-flat-form"style="
+width:80% ;
+    background: #BFC9CA;
+    color: white;
+    padding: 20px;
+    margin: 5%;
+    border-radius: 20px;
+    ">
+    <!--Capitulos, Nombres y portadas-->
+<center>
+<a href="1.php" style="color:black;">
+<h3><img src="../../img/carpetadelanime/name.jpg" class="imgc">Nombre del anime</h3>
+capituli 1
+</a>
+</center>
+<br>
+<center>
+<a href="2.php" style="color:black;">
+<h3><img src="../../img/carpetadelanime/name.jpg" class="imgc">Nombre del anime</h3>
+capitulo 2
+</a>
+</center>
+
+<br>
+<center>
+<a href="3.php" style="color:black;">
+<h3><img src="../../img/carpetadelanime/name.jpg" class="imgc">Nombre del anime</h3>
+capitulo 3
+</a>
+</center>
+<br>
+<center>
+<a href="4.php" style="color:black;">
+<h3><img src="../../img/carpetadelanime/name.jpg" class="imgc">Nombre del anime</h3>
+capitulo 4
+</a>
+</center>
+<br>
+<center>
+<a href="5.php" style="color:black;">
+<h3><img src="../../img/carpetadelanime/name.jpg" class="imgc">Nombre del anime</h3>
+capitulo 5
+</a>
+</center>
+<br>
+<center>
+<a href="6.php" style="color:black;">
+<h3><img src="../../img/carpetadelanime/name.jpg" class="imgc">Nombre del anime</h3>
+capitulo 6
+</a>
+</center>
+<br>
+<center>
+<a href="7.php" style="color:black;">
+<h3><img src="../../img/carpetadelanime/name.jpg" class="imgc">Nombre del anime</h3>
+capitulo 7
+</a>
+</center>
+<br>
+<center>
+<a href="8.php" style="color:black;">
+<h3><img src="../../img/carpetadelanime/name.jpg" class="imgc">Nombre del anime</h3>
+capitulo 8
+</a>
+</center>
+<br>
+<center>
+<a href="9.php" style="color:black;">
+<h3><img src="../../img/carpetadelanime/name.jpg" class="imgc">Nombre del anime</h3>
+capitulo 9
+</a>
+</center>
+<br>
+<center>
+<a href="10.php" style="color:black;">
+<h3><img src="../../img/carpetadelanime/name.jpg" class="imgc">Nombre del anime</h3>
+capitulo 10
+</a>
+</center>
+<br>
+<center>
+<a href="11.php" style="color:black;">
+<h3><img src="../../img/carpetadelanime/name.jpg" class="imgc">Nombre del anime</h3>
+capitulo 11
+</a>
+</center>
+<br>
+<center>
+<a href="12.php" style="color:black;">
+<h3><img src="../../img/carpetadelanime/name.jpg" class="imgc">Nombre del anime</h3>
+capitulo 12
+</a>
+</center>
+<br>
+<center>
+<a href="13.php" style="color:black;">
+<h3><img src="../../img/carpetadelanime/name.jpg" class="imgc">Nombre del anime</h3>
+capitulo 13
+</a>
+</center>
+<br>
+<center>
+<a href="14.php" style="color:black;">
+<h3><img src="../../img/carpetadelanime/name.jpg" class="imgc">Nombre del anime</h3>
+capitulo 14
+</a>
+</center>
+<br>
+<center>
+<a href="15.php" style="color:black;">
+<h3><img src="../../img/carpetadelanime/name.jpg" class="imgc">Nombre del anime</h3>
+capitulo 15
+</a>
+</center>
+<br>
+</div>
+
+
+
+        </div>
+        <footer class="footer full-reset">
+
+            <div class="footer-copyright full-reset all-tittles">Mundo anime</div>
+        </footer>
+    </div>
+</body>
+</html>
