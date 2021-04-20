7121cbf2bbd6a68e667aa95115219e6a1a7c841d
samuelangeles <samueljoseangelereye@gmail.com>
2021-04-20 11:50:06 -0600

    mortal

--- paginaanime/mortal/11.php
+++ paginaanime/mortal/11.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <title>Peliculas</title>
+    <title>Mortal King</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="Shortcut Icon" type="image/x-icon" href="assets/icons/logoo.png" />
