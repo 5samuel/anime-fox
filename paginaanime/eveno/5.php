4d3c8b2b94164dd888c89c04aab78fadc6a6738a
JeffersonHidalgo <jeffersonhidalgo@gmail.com>
2021-05-10 23:11:41 -0400

    EveNoJikan

---
+++ paginaanime/eveno/5.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <!--Aqui va el titulo de la pestaña colocar nombre del anime-->
+    <title>Eve No Jikan</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="Shortcut Icon" type="image/x-icon" href="assets/icons/logoo.png" />
+    <script src="js/sweet-alert.min.js"></script>
+    <link rel="stylesheet" href="../../css/boton.css">
+    <link rel="stylesheet" href="../../css/sweet-alert.css">
+    <link rel="stylesheet" href="../../css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" href="../../css/normalize.css">
+    <link rel="stylesheet" href="../../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../css/jquery.mCustomScrollbar.css">
+    <link rel="stylesheet" href="../../css/style.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/jquery-1.11.2.min.js"><\/script>')</script>
+    <script src="../../js/modernizr.js"></script>
+    <script src="../../js/bootstrap.min.js"></script>
+    <script src="../../js/jquery.mCustomScrollbar.concat.min.js"></script>
+    <script src="../../js/main.js"></script>
+</head>
+
+
+<style>
+
+    .contenedor{
+    position:relative;
+    width:600px;
+    height:390px;
+    padding-left: 20px;
+    padding-top: 30px;
+    background-color:#343437;
+    border-radius: 50px;
+    margin:auto;
+}
+
+
+</style>
+<body>
+    <div class="navbar-lateral full-reset">
+        <div class="visible-xs font-movile-menu mobile-menu-button"></div>
+        <div class="full-reset container-menu-movile custom-scroll-containers">
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+            <div class="full-reset" style="background-color:#2B3D51; padding: 10px 0; color:#fff;">
+                <figure>
+                    <img src="assets/img/logoo.png" alt="Biblioteca" class="img-responsive center-box" style="width:55%;">
+                </figure>
+                <p class="text-center" style="padding-top: 15px;"><font size="4" face="Algerian">Peliculas</font></p>
+            </div>
+
+            <!--Aqui va el menu todavia no se ha terminado asi que no preocuparce por el momento-->
+
+            <div class="full-reset nav-lateral-list-menu">
+                <ul class="list-unstyled">
+                    <li><a href="../../inicio.html"><i class="zmdi zmdi-home zmdi-hc-fw"></i>&nbsp;&nbsp; Inicio</a></li>
+                  
+                             <li><a href="desconectar.php"><i class="zmdi zmdi-movie"></i>&nbsp;&nbsp;  Cerrar</a></li>
+                
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="content-page-container full-reset custom-scroll-containers">
+
+        <!-- aqui va el titulo sipnosis y demas datos-->
+
+    	 <div class="footer-copyright full-reset all-tittles">Mundo del anime</div>
+
+        <div class="container">
+            <div class="page-header">
+              <h1 class="all-tittles"><center><font size="10" face="Algerian"> Episodio  5</font></center></h1>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <ul class="nav nav-tabs nav-justified"  style="font-size: 17px;">
+
+            </ul>
+        </div>
+        <div class="container-fluid" >
+
+<div class="container-flat-form" style="height:200%;">
+<br>
+
+<div class="contenedor" style="height:200%;">
+<!--Aqui dentro va el video y los botones-->
+
+<div style="position:relative;height:0;padding-bottom:56.6%"><iframe src="https://streamium.xyz/gocdn.html#elB3VGIxNzhIK0IweGM4SE1DZWNlVWtMM2dhellPSS9QQXZWelUvQm5aNWhrQVZjejUzNmF1bFkyRG9aUHA4a1cwWTYwRm9keGgrTEhBVjIrZDRsMW8xa1JqemZlc1hLeFRwcklYQzR3Rjg9" controls style="position:absolute;width:97%;height:100%;left:0; border-radius: 10px;" width="500" height="390" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>  </div>
+<br>
+</div>
+<!-- Botones -->
+<br>
+<a href="4.php"><button style="float-right:30px;">Anterior</button></a>
+<a href="inicio.php"><button style="float-right:30px;">capitulos</button></a>
+<a href="5.php"><button style="float-right:30px;">Siguiente</button></a>
+
+<br>
+</div>
+
+        </div>
+        <footer class="footer full-reset">
+
+            <div class="footer-copyright full-reset all-tittles">Comentario.</div>
+        </footer>
+      
+        
+        
+        <div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v10.0" nonce="jPdBhHZW"></script>
+
+<div class="fb-comments" data-href="http://localhost/samuel/trabajo/peliculas/inicio.php" data-width="" data-numposts="100"></div>
+
+    </div>
+</body>
+</html>
